3696064943a6e8d750f0803cf786dae4bb37c00c
JakubSlomczewski <s25505@pjwstk.edu.pl>
2025-03-26 20:17:55 +0100

    1.Uzupełnienie klasy Statek i spełnienie wymagań zadania. 2. Dodanie metod obsługi statku 3. Wstępne testowanie programu w klasie "Program"

--- Zadania/Program.cs
+++ Zadania/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Zadania;
+
+Console.WriteLine("=== SYMULACJA SYSTEMU KONTENEROWEGO ===\n");
+
+        
+        var statek1 = new Statek("Posejdon", 25, 5, 60_000); 
+        var statek2 = new Statek("Neptun", 20, 3, 40_000);
+
+
+        var kontenerC = new KontenerC(0, 240, 2000, 600, "", 10000, "Bananas", 12);
+        var kontenerG = new KontenerG(0, 240, 2500, 600, "", 8000, 5.5);
+        var kontenerL = new KontenerL(0, 240, 1800, 600, "", 9000, true);
+
+        
+        try
+        {
+            kontenerC.LoadPayload(5000); 
+            kontenerG.LoadPayload(7000); 
+            kontenerL.LoadPayload(4000); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("[ERROR] " + ex.Message);
+        }
+
+  
+        statek1.AddKontener(kontenerC);
+        statek1.AddKontener(kontenerG);
+        statek1.AddKontener(kontenerL);
+
+       
+        Console.WriteLine("\n--- Informacje o statku Posejdon ---");
+        statek1.StatekInfo();
+
+        
+        var kontenerL2 = new KontenerL(0, 220, 1900, 600, "", 8000, false);
+        kontenerL2.LoadPayload(6000); 
+
+        
+        statek2.AddKontener(kontenerL2);
+
+        Console.WriteLine("\n--- Informacje o statku Neptun ---");
+        statek2.StatekInfo();
+
+        
+        Statek.MoveKontener(kontenerG, statek1, statek2);
+
+        Console.WriteLine("\n--- Po przeniesieniu kontenera ---");
+        statek1.StatekInfo();
+        statek2.StatekInfo();
+
+        var kontenerZaDuzy = new KontenerC(0, 240, 30_000, 600, "", 40_000, "Fish", 5);
+        kontenerZaDuzy.LoadPayload(15_000);
+
+        statek2.AddKontener(kontenerZaDuzy);
+
+        Console.WriteLine("\n=== KONIEC TESTU ===");
--- Zadania/statek.cs
+++ Zadania/statek.cs
@@ -1,6 +1,71 @@
 namespace Zadania;
 
-public class statek
+public class Statek
 {
+    public string Name { get; set; }
+    public int Speed { get; set; }
+    public int MaxNumberOfContainters { get; set; }
+    public int MaximumWeightOfContainters { get; set; }
+    public List<Kontener> Kontenery { get; set; }
+
+    public Statek(string name, int speed, int numberOfContainters, int maximumWeightOfContainters)
+    {
+        Name = name;
+        Speed = speed;
+        MaxNumberOfContainters = numberOfContainters;
+        MaximumWeightOfContainters = maximumWeightOfContainters;
+        Kontenery = new List<Kontener>();
+    }
+
+    public void AddKontener(Kontener kontener)
+    {
+        double currentWeight = Kontenery.Sum(k => k.DeadWeight + k.Payload);
+        double weightAfterAddingContainer = kontener.Payload + kontener.DeadWeight + currentWeight;
+
+        if (MaximumWeightOfContainters < weightAfterAddingContainer / 1000)
+        {
+            Console.WriteLine("Nie można dodać kontenera – przekroczono limit wagowy");
+            
+        }else if (MaxNumberOfContainters < Kontenery.Count)
+        {
+            Console.WriteLine("Nie można dodać kontenera – przekroczono limit liczby kontenerów.");
+        }
+        else
+        {
+          Kontenery.Add(kontener);  
+          Console.WriteLine("Dodano Kontener "+ kontener.SerialNumber + " do statku "+ Name);
+        }
+    }
+
+    public void RemoveKontener(Kontener kontener)
+    {
+        Kontenery.Remove(kontener);
+        Console.WriteLine("Usunięto kontener "+ kontener.SerialNumber + " ze statku "+ Name);
+    }
+
+
+    public static void MoveKontener(Kontener kontener, Statek z, Statek doStatku)
+    {
+        doStatku.AddKontener(kontener);
+        z.RemoveKontener(kontener);
+        Console.WriteLine("Przeniesiono kontener ze statku "+ z.Name +" do statku "+ doStatku.Name);
+    }
+
+
+    public void StatekInfo()
+    {
+        Console.WriteLine($"Statek: {Name}");
+        Console.WriteLine($"Prędkość maksymalna: {Speed} węzłów");
+        Console.WriteLine($"Kontenery ({Kontenery.Count}/{MaxNumberOfContainters}):");
+        foreach (var k in Kontenery)
+        {
+            Console.WriteLine($"  {k}");
+        }
+
+        double lacznaWaga = Kontenery.Sum(k => k.DeadWeight + k.Payload) / 1000;
+        Console.WriteLine($"Aktualna łączna waga: {lacznaWaga} ton (max: {MaximumWeightOfContainters} ton)");
+    }
+    
+    
     
 }
